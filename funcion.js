84942c1f08afda6632836aba8dfbb491efa53ed8
jimechavez <jimena1997ch@gmail.com>
2022-11-29 23:09:57 -0300

    agrego archivos

---
+++ funcion.js
@@ -0,0 +1,20 @@
+
+let pais = "España";
+
+let continente = "Europa";
+
+let antiguedad = "2015";
+
+let pais_y_continente = pais + " " + continente;
+
+pais = "Mexico";
+continente = "Latinoamerica";
+
+console.log(pais,continente,antiguedad);
+alert(pais_y_continente);
+
+
+
+
+
+
